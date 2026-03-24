4fddf6b7789a63a128862515c43bfc7aae9aba7d
IronTunder <matteoone75@gmail.com>
2026-03-24 01:22:18 +0100

    Return to basic frontend

--- frontend/ditto-app/app/page.tsx
+++ frontend/ditto-app/app/page.tsx
@@ -1,15 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { useAuth } from "@/contexts/AuthContext";
 
 export default function index() {
-  const { user, machine, isAuthenticated, isLoading, logout } = useAuth();
   const router = useRouter();
-
-  useEffect(() => {
-    if (!isLoading && !isAuthenticated) {
-      router.push("/operator/login");
-    }
-  }, [isLoading, isAuthenticated, router]);
 }
